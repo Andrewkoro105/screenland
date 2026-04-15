db186045b941ec7e1faf451de7484031766fe9b6
Andrew Korovkin <andrewkoro105@gmail.com>
2026-04-15 12:44:54 +0300

    feat: add a Nix package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Development shell for my Rust app";
+  description = "Screenland is a program for creating and editing screenshots";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -19,25 +19,58 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in
-      {
-        nixpkgs.config.allowUnfree = true;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            pkg-config
-            rust-bin.stable.latest.default
-          
+        buildInputs = with pkgs; [
             libxkbcommon
             wayland
             libX11
             libXcursor
             libXrandr
             libXi
-            alsa-lib
-            fontconfig
-            freetype
+            libGL
+            vulkan-loader
+            vulkan-validation-layers
+            mesa
 
+            vulkan-tools
+          ];
+      in
+      {
+        nixpkgs.config.allowUnfree = true;
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "screenland";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "iced_helper-0.1.0" = "sha256-Uxrv0YilON+pOEDaJBltjltU5cTmO2d3bdvIWRh/Zts=";
+              "iced-0.14.0" = "sha256-hCK2QHrhGdwWaioa0hI4niHTad39g3mYsZe8ltcDXxY=";
+              "iced_exdevtools-0.16.0" = "sha256-ITy16MwervR9vDig0kJZOtS6czXz2x2Xhx4YfsmhB40=";
+            };
+          };
+
+          nativeBuildInputs = with pkgs; [ pkg-config makeWrapper ];
+
+          inherit buildInputs;
+
+          postInstall = ''
+            wrapProgram $out/bin/screenland \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath buildInputs}
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            rust-bin.stable.latest.default
+
+            libxkbcommon
+            wayland
+            libX11
+            libXcursor
+            libXrandr
+            libXi
             libGL
             vulkan-loader
             vulkan-validation-layers
